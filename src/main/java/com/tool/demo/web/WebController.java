171723587ae766adea8da14279a9cf1588c95350
gitsxlx <liferaytest@163.com>
2020-08-24 17:31:53 +0800

    spring属性和方法注入

--- src/main/java/com/tool/demo/web/WebController.java
+++ src/main/java/com/tool/demo/web/WebController.java
@@ -2,7 +2,10 @@ package com.tool.demo.web;
 
 import com.tool.demo.framework.log.Log;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +30,9 @@ public class WebController {
 
     private static String contextPath1;
 
+    @Value("${server.servlet.context-path:YuLinYun}")
+    private static String contextPath2;
+
     /**
      * 注意：
      * spring不允许/不支持把值注入到静态变量中；
@@ -38,6 +44,10 @@ public class WebController {
      * 一旦你使用静态方法，就不再需要去产生这个类的实例，这会让testing变得更加困难，同时你也不能为一个给定的类，依靠注入方式去产生多个具有不同的依赖环境的实例。
      * 这种static field是隐含共享的，并且是一种global全局状态，spring同样不推荐这样去做。
      *
+     * 源码：
+     * @see AutowiredAnnotationBeanPostProcessor#buildAutowiringMetadata 方法获取到需要注入的属性
+     * @see AutowiredAnnotationBeanPostProcessor#inject 方法获取属性值、注入
+     *
      * @param contextPath1
      */
     @Value("${server.servlet.context-path:YuLinYun}")
@@ -45,11 +55,16 @@ public class WebController {
         WebController.contextPath1 = contextPath1;
     }
 
+    public void setContextPath2(String contextPath2) {
+        WebController.contextPath2 = contextPath2;
+    }
+
     @Log("默认页")
     @GetMapping("/")
     public String index() {
-        log.info("默认页, applicationName:{}, contextPath:{}, contextPath1:{}", applicationName,
-                contextPath, contextPath1);
+        //默认页, applicationName:MyTools, contextPath:null, contextPath1:/tools, contextPath2:null
+        log.info("默认页, applicationName:{}, contextPath:{}, contextPath1:{}, contextPath2:{}",
+                applicationName, contextPath, contextPath1, contextPath2);
         return "welcome to MyTools";
     }
 
